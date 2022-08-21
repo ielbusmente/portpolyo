3a856263adf74f11b24cdfcb217f8b6af1afff8a
ielbusmente <ieljbusmente@gmail.com>
2022-08-21 15:08:00 +0800

    try build fix

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Login from "../modules/Admin/index"
-import Dashboard from "../modules/Admin/Dashboard"
-import Logs from "../modules/Admin/Logs/Logs"
-import Projects from "../modules/Admin/Projects/Projects"
-import Project_Type from "../modules/Admin/Project_Type/Project_Type"
+import Dashboard from "../modules/Admin/Dashboard.js"
+import Logs from "../modules/Admin/Logs/Logs.js"
+import Projects from "../modules/Admin/Projects/Projects.js"
+import Project_Type from "../modules/Admin/Project_Type/Project_Type.js"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Skill_Types from "../modules/Admin/Skill_Types/Skill_Types"
-import Skills from "../modules/Admin/Skills/Skills"
+import Skill_Types from "../modules/Admin/Skill_Types/Skill_Types.js"
+import Skills from "../modules/Admin/Skills/Skills.js"
 
 export default function Site() {
   return (
